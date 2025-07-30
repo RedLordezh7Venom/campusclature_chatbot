690b69768e2974555d4b78c43204dc5bb4ca52df
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-07-30 21:22:10 +0530

    chat_model made

--- chatbot/chat_model.py
+++ chatbot/chat_model.py
@@ -3,11 +3,27 @@ from langchain.schema import SystemMessage, HumanMessage
 from langchain.prompts import PromptTemplate
 from prompts.campus_waifu import prompt_template
 
+from rag.retriever import vector_store
+from chatbot.memory_manager import memory
+import os
+from dotenv import load_dotenv
+load_dotenv()
+#setting up our llm
+api_key = os.getenv("QROQ_API_KEY")
+
+llm = ChatGroq(
+    model='deepseek-r1-distill-llama-70b',
+    reasoning_format="hidden",
+    api_key = api_key,
+    max_tokens = 512
+)
+
+#prompt
 prompt = PromptTemplate(
     input_variables=["context", "question"],
     template=prompt_template
 )
-
+#main chain
 from langchain.chains import ConversationalRetrievalChain
 
 qa_chain = ConversationalRetrievalChain.from_llm(

c062b6cea326d1a5d51216f5c425272a4edca5cf
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-07-30 22:06:34 +0530

    added doc loader

--- rag/document_loader.py
+++ rag/document_loader.py
@@ -1,1 +1,12 @@
-# Loads documents for RAG
+from langchain.document_loaders import PyPDFLoader
+pdf_path = "/content/cbse_courses_dataset.pdf"
+# Load PDF with PyPDFLoader
+loader = PyPDFLoader(pdf_path)
+pages = loader.load()
+
+
+if __name__ == "__main__":
+    # View content of the first few pages
+    for i, page in enumerate(pages[:2]):  # Adjust range if you want more pages
+        print(f"\n--- Page {i+1} ---\n")
+        print(page.page_content)
